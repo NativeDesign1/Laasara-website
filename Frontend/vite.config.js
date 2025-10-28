aa7720a3990d0f8321c78e91cdbdccb8212287ab
unknown <oualid.safir@outlook.com>
2025-10-28 15:47:24 +0100

    Newest commit, latest changes

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -3,11 +3,15 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    sourcemap: true
+  },
   server: {
     port: 3000,
-    host: 'localhost',
-    open: true,
-    cors: true, // Enable CORS
+    host: true,
+    open: true
   },
   esbuild: {
     loader: "jsx",
